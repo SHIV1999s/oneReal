f5431ee10d246d53e6f9099fefc7e358bea553b4
Shivpratap Singh <shivjayprakashsingh@gmail.com>
2025-09-05 23:13:25 +0530

    one real round 2

--- pom/boldRealpom.js
+++ pom/boldRealpom.js
@@ -1,69 +1,82 @@
-// export class boldRealpom {
-//     constructor(page) {
-//         this.page = page;
-//         // await page.waitForTimeout(2000)
-//         // await 
-//         this.clickTransaction = page.locator('text=KJ1-T94-26Y-23O').click({ force: true })
-//         // await page.locator('[href*="Onboardings"] [type="button"]').first().click();
-//         this.details = page.locator('[class="relative"] [class*="px-4 flex flex-row"]').locator('text=Details')
-//         this.clickTransaction = this.details.scrollIntoViewIfNeeded();
-//         await details.click();
-//         await page.locator('text="Adjust Commission (%) / splits"').scrollIntoViewIfNeeded();
-//         await page.locator('text="Adjust Commission (%) / splits"').click();
-//     }
-//     async goToUrl() {
-//         await page.goto('https://bolt.playrealbrokerage.com/');
-//         await page.waitForLoadState('domcontentloaded')
-//     };
-//     async goToTogglePage() {
-//         // await page.waitForTimeout(2000)
-//         // await 
-//         await page.locator('text=KJ1-T94-26Y-23O').click({ force: true })
-//         // await page.locator('[href*="Onboardings"] [type="button"]').first().click();
-//         const details = page.locator('[class="relative"] [class*="px-4 flex flex-row"]').locator('text=Details')
-//         await details.scrollIntoViewIfNeeded();
-//         await details.click();
-//         await page.locator('text="Adjust Commission (%) / splits"').scrollIntoViewIfNeeded();
-//         await page.locator('text="Adjust Commission (%) / splits"').click();
+export class boldRealpom {
+    constructor(page, expect) {
+        this.page = page;
+        this.expect = expect;
 
-//     };
-//     async test1() {
-//         const togglesPage = page.locator('[title="sidebar-form"]');
-//         const toggle = await togglesPage.locator('[data-testid="toggle-switch-button"]');
-//         await toggle.first().click();
-//         await toggle.nth(1).click();
-//         await page.locator('[class*="p-4 border-b"] [name*="money.amount"]').first().fill('50000');
-//         await page.locator('[class*="p-4 border-b"] [name*="money.amount"]').nth(1).fill('50000');
-//         const total = await page.locator('[class="font-inter text-sm font-medium"]').textContent();
-//         console.log(total);
-//     }
-//     async test2() {
-//         //pay in % test 2
-//         for (let i = 0; i < 4; i++) {
-//             await toggle.nth(i).click();
-//             await page.getByTestId(`commissionParticipant[${i}].percent.value`).fill('25')
-//         }
-//         //    await page.pause();
-//         console.log(total);
+        this.url = 'https://bolt.playrealbrokerage.com/';
+        this.transaction = page.locator('text=KJ1-T94-26Y-23O');
+        this.details = page.locator('[class="relative"] [class*="px-4 flex flex-row"]').locator('text=Details');
+        this.commision = page.locator('text="Adjust Commission (%) / splits"');
+        this.togglePage = '.mb-28 section'
+        this.perTextBox = page.locator('[placeholder="Percentage"]:visible');
+        this.amtTextBox = page.locator('[placeholder="Amount"]:visible')
+        this.toggleBtn = 'button[data-testid*="toggle"]';
+        this.perToggle = page.locator(this.togglePage).filter({ has: this.perTextBox }).locator(this.toggleBtn);
+        this.verifyTotalAmt = page.locator('[class="flex justify-end"]');
+        this.perAmt = '$200,000 (100.00%) / $200,000';
+        this.amount = '$200,000 / $200,000';
+        this.amtToggle = page.locator(this.togglePage).filter({ has: this.amtTextBox }).locator(this.toggleBtn);
+    }
+    async goToUrl() {
+        await this.page.goto(this.url);
+        await this.page.waitForLoadState('domcontentloaded')
+    }
+    async goToCommisionPage() {
+        await this.transaction.click({ force: true })
+        await this.details.scrollIntoViewIfNeeded();
+        await this.details.click();
+        await this.commision.scrollIntoViewIfNeeded();
+        await this.commision.click();
+    };
+    async test1() {
+        const disableTogglePer =await this.disableTogglePer();
+        // test 1 all use split into $
+        for (let i = 0; i < (await disableTogglePer).length; i++) {
+            await (await disableTogglePer)[0].click();
+            await this.amtTextBox.nth(i).fill("50000");
+        }
+        await this.expect(this.verifyTotalAmt).toHaveText(this.perAmt);
+        await this.expect(this.verifyTotalAmt).toContainText(this.perAmt);
+        this.expect(await this.verifyTotalAmt.textContent()).toMatch(/^.{17,}$/);
+        // await this.page.pause();
+    }
+    async disableToggleAmt() {
+        return this.amtToggle.all()
+    }
+    async disableTogglePer() {
+        return this.perToggle.all()
+    }
+    async test2() {
+        // test 2 all use split into %
+        const disableToggleAmt =await this.disableToggleAmt();
+        for (let i = 0; i < disableToggleAmt.length; i++) {
+            // console.log((await this.disableToggleAmt()).length);
+            
+            await disableToggleAmt[0].click();
+            await this.perTextBox.nth(i).fill("25");
+        };
+        await this.expect(this.verifyTotalAmt).toHaveText(this.amount);
+        await this.expect(this.verifyTotalAmt).toContainText(this.amount);
+        this.expect(await this.verifyTotalAmt.textContent()).toMatch(/^[0-9$/, ]{19,}$/);
+        // await this.page.pause();
 
-//     }
-//     async test3() {
-//         //test 3 1st 2 paid in$  2nd 2 paid in %
-//         for (let i = 0; i < 2; i++) {
-//             await toggle.nth(i).click();
-//             await page.locator('[class*="p-4 border-b"] [name*="money.amount"]').nth(i).fill('50000');
-//         };
-
-//         for (let i = 2; i < 4; i++) {
-//             await page.getByTestId(`commissionParticipant[${i}].percent.value`).fill('50')
-//         }
-//         console.log(total);
-//         await page.pause()
-//     }
-// }
-
-
-
+    }
+    async test3() {
+        // test 3 all use split into % and & 2:2 ratio;
+        const disableTogglePer =await this.disableTogglePer();
+        for (let i = 0; i < 2; i++) {
+            console.log(i);
+            await disableTogglePer[0].click();
+            await this.amtTextBox.nth(i).fill("50000");
+        }
+        for (let i = 0; i < (await this.perTextBox.all()).length; i++) {
+            await this.perTextBox.nth(i).fill("50");
+        };
+        await this.expect(this.verifyTotalAmt).toHaveText(this.amount);
+        await this.expect(this.verifyTotalAmt).toContainText(this.amount);
+        this.expect(await this.verifyTotalAmt.textContent()).toMatch(/^[0-9$/, ]{19,}$/);
+    }
+}
 
 
 
